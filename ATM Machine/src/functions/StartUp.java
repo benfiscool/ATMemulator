859d954c190d4bb19c2e03e281aca8c9968c59a3
nysfour <43475563+nysfour@users.noreply.github.com>
2018-10-25 07:45:21 -0700

    Update ATM Machine/src/functions/StartUp.java

--- ATM Machine/src/functions/StartUp.java
+++ ATM Machine/src/functions/StartUp.java
@@ -19,7 +19,7 @@ public class StartUp {
 		System.out.println("--------------------------------------\n|  Benny's Bank                      |\n|  Where banking is done right       |\n|  "
 				+ "Today's Date: " + dtf.format(now) +  "          |\n--------------------------------------");  
 	}
-	public static void menuSelect() throws Exception {
+public static void menuSelect() throws Exception {
 		System.out.print("\nPlease select one of the following options:\n1.Create new account\n2.Sign in to an existing account\n");
 		System.out.print("Please enter a selection: ");
         if(scan.hasNextInt()){
@@ -39,7 +39,7 @@ public class StartUp {
             Retry();
         }
 		
-	}
+	} 
 	public static void Retry() throws Exception {
         if(scan.hasNextInt()){
         	int selection = scan.nextInt();
